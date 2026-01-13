9a197c69ec2e66c4a45195662a5ff2ccfb87a4cb
Sammed Badde <sammedbadde10@gmail.com>
2026-01-13 11:06:44 +0530

    Implement sum calculation in Question_1.c

--- Assignment No 0/Question_1.c
+++ Assignment No 0/Question_1.c
@@ -1,1 +1,11 @@
+#include<stdio.h>
+int main() {
+int number1=10;
+int number2=20;
+int sum;
 
+sum= number1+number2;
+    printf("The sum of %d and %d is: %d\n", number1, number2, sum);
+
+    return 0;
+}
