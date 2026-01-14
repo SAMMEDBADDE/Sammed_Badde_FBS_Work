3bb0c652045c8a5faf1baabfd3598ad0ffdf4bf7
Sammed Badde <sammedbadde10@gmail.com>
2026-01-14 16:37:04 +0530

    Implement circle area calculation program

--- Assignment No 0/Question_2.c
+++ Assignment No 0/Question_2.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main() {
+    float radius, area;
+    const float PI = 3.14159; 
+    printf("Enter the radius of circle : ");
+    scanf("%f", &radius); 
+    area = PI * radius * radius;
+    printf("The area of circle is : %.2f\n", area);
+    return 0;
+}
