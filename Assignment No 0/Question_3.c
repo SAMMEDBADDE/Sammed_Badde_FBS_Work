d791b15066eeee760ce0d188bccb08d52f034d21
Sammed Badde <sammedbadde10@gmail.com>
2026-01-17 20:01:15 +0530

    Update Question_3.c

--- Assignment No 0/Question_3.c
+++ Assignment No 0/Question_3.c
@@ -1,1 +1,8 @@
-
+#include<stdio.h>
+int main()
+{
+    float celsius=10, fahrenheit;
+    fahrenheit=(celsius*9/5)+32;
+    printf("temperature in fahrenheit : %4f\n",fahrenheit);
+    return 0;
+}
