fe640a9269ccb77bdd09df063cab6cf306bd2a63
Sammed Badde <sammedbadde10@gmail.com>
2026-01-18 20:38:19 +0530

    Create Question_5.c

---
+++ Assignment No 0/Question_5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+
+{
+    int a=12, b=14, c=16, d=18, e=20;
+    int sum;
+    float avg;
+    sum=a+b+c+d+e;
+    avg= sum/5.0;
+    printf("Average of these numbers is : %2f",avg);
+
+}
