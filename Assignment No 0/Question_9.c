bf8cce0e8931967d53052b756b3e58e8edd9d400
Sammed Badde <sammedbadde10@gmail.com>
2026-01-20 08:02:44 +0530

    Create Question_9.c

---
+++ Assignment No 0/Question_9.c
@@ -0,0 +1,11 @@
+//Write a C program to input the base and height of a triangle and calculate its area.
+  #include <stdio.h>
+int main()
+{
+
+    int base = 20;
+    int height = 10;
+    float area = 0.5 * base * height;
+    printf("Area of triangle is : %.2f", area);
+    return 0;
+}
