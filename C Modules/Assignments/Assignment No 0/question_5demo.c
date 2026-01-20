24de06da59668c6492a8c1bafcefb5c1bf041b9f
Sammed Badde <sammedbadde10@gmail.com>
2026-01-20 19:47:49 +0530

    Add files via upload

---
+++ C Modules/Assignments/Assignment No 0/question_5demo.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int sub1=12;
+    int sub2=14;
+    int sub3=16;
+    int sub4=18;
+    int sub5=20;
+    int total = sub1+sub2+sub3+sub4+sub5;
+    float avg =total/5.0;
+    printf("Avg is : %f",avg);
+}
