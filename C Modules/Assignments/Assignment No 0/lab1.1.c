146db3b87694fae023999308409fa7d12aabfa24
Sammed Badde <sammedbadde10@gmail.com>
2026-01-20 08:47:50 +0530

    Add files via upload

---
+++ C Modules/Assignments/Assignment No 0/lab1.1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int no=1;
+    if(no%2==0)
+    {
+        printf("No is Even");
+    
+    }
+    else{
+        printf("No is odd");
+    }
+    return 0;
+}
