67f6af554caf9ed96567a9e63482e85901349dc0
Sammed Badde <sammedbadde10@gmail.com>
2026-01-20 08:15:02 +0530

    Create Question_10.c

---
+++ Assignment No 0/Question_10.c
@@ -0,0 +1,19 @@
+//Write a C program to input marks of five subjects, find the total marks, and calculate
+the percentage.
+#include <stdio.h>
+int main()
+{
+    int s1 = 20;
+    int s2 = 20;
+    int s3 = 20;
+    int s4 = 20;
+    int s5 = 50;
+
+    int total = s1 + s2 + s3 + s4 + s5;
+    float per = (total / 500.0) * 100;
+
+    printf("Total of marks :%.d\n", total);
+    printf("Percentage of total is :%.2f", per);
+
+    return 0;
+}
