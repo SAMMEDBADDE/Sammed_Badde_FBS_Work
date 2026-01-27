d059bce88291abd2e4070615c6d7d00714f27184
Sammed Badde <sammedbadde10@gmail.com>
2026-01-27 08:09:22 +0530

    Update question_6.c

--- C Modules/Assignments/Assignment No 2/question_6.c
+++ C Modules/Assignments/Assignment No 2/question_6.c
@@ -1,7 +1,6 @@
 // Accept a number and check if it is divisible by 3, 5, or both.
 // (Print "Divisible by 3 but not by 5" or "Divisible by 5 but not by 3" or "Divisible by
 // both" or” Divisible by None”)
-
 #include <stdio.h>
 int main()
 {
@@ -27,5 +26,6 @@ int main()
     printf("Divisible by none");
 } 
 }
+
 
 //jar user ne char enter kela tar Invalid yeude 
