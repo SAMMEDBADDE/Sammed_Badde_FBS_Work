38996dd692164297fc7b2fece55e76cfdba2ff94
Sammed Badde <sammedbadde10@gmail.com>
2026-02-02 15:36:02 +0530

    Add files via upload

---
+++ C Modules/Assignments/Assignment No 3/for_loop.c
@@ -0,0 +1,13 @@
+//take value (n) from user and print any statement n times.
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number : ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        printf("Hello Iam Sammed\n");
+    }
+}
