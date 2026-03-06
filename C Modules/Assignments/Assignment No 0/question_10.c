e728c91da7a7900ba56f8387d083373473bca423
Sammed Badde <sammedbadde10@gmail.com>
2026-03-06 09:55:25 +0530

    Update question_10.c

--- C Modules/Assignments/Assignment No 0/question_10.c
+++ C Modules/Assignments/Assignment No 0/question_10.c
@@ -8,13 +8,14 @@ int main()
     int s5 = 50;
 
   int total = s1 + s2 + s3 + s4 + s5;
- float per = (total / 500.0) * 100;
+  float per = (total / 500.0) * 100;
 
     printf("Total of marks :%.d\n", total);
     printf("Percentage of total is :%.2f", per);
 
     return 0;
 }
+
 
 
 
