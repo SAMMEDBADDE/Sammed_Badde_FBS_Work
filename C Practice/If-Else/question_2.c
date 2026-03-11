ce081fab33e49e6ceb4ef34ea99aec279240093c
Sammed Badde <sammedbadde10@gmail.com>
2026-03-11 08:10:48 +0530

    Update question_2.c

--- C Practice/If-Else/question_2.c
+++ C Practice/If-Else/question_2.c
@@ -2,17 +2,18 @@
 #include <stdio.h>
 int main()
 {
-     int n;
-     printf("Enter a number : ");
-     scanf("%d", &n);
-    if (n % 5 == 0)
-    {
-        printf("Divisible by 5");
-        }
-        else {
-        printf("Not Divisible by 5");
+ int n;
+ printf("Enter a number : ");
+ scanf("%d", &n);
+ if (n % 5 == 0)
+{
+      printf("Divisible by 5");
+       }
+      else {
+      printf("Not Divisible by 5");
         }
 }
+
 
 
 
