bb735c98b27e686d7834a747f03a989d2809a738
Sammed Badde <sammedbadde10@gmail.com>
2026-03-17 09:05:59 +0530

    Update practice_3.c

--- C Practice/Function/practice_3.c
+++ C Practice/Function/practice_3.c
@@ -5,8 +5,8 @@ return (x*y);
 int main()
 {
     int a;
-    printf("Enter 1st Number : ");
-    scanf("%d", &a);
+     printf("Enter 1st Number : ");
+     scanf("%d", &a);
     int b;
     printf("Enter 2st Number : ");
     scanf("%d", &b);
