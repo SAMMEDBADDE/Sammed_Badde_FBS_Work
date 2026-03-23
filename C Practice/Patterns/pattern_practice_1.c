3c44fe12951a52dd5db3028438c6cae2c920db67
Sammed Badde <sammedbadde10@gmail.com>
2026-03-23 09:23:44 +0530

    Update pattern_practice_1.c

--- C Practice/Patterns/pattern_practice_1.c
+++ C Practice/Patterns/pattern_practice_1.c
@@ -3,7 +3,7 @@ int main()
 {
     for (int row = 1; row <= 4; row++)
     {
-        for (int col = 1; col <= 4; col++)
+       for (int col = 1; col <= 4; col++)
         {
         printf("* ");
         }
