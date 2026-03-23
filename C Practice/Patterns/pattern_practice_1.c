6422f019387bdf1b4b5b6d56a5f95fb62fb23e6a
Sammed Badde <sammedbadde10@gmail.com>
2026-03-23 09:23:10 +0530

    Update pattern_practice_1.c

--- C Practice/Patterns/pattern_practice_1.c
+++ C Practice/Patterns/pattern_practice_1.c
@@ -6,8 +6,8 @@ int main()
         for (int col = 1; col <= 4; col++)
         {
 
-          printf("* ");
+        printf("* ");
         }
-           printf("\n");
+        printf("\n");
     }
 }
