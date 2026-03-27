9bf6ddc8dd084ead692dba6415756dd7b2e75f70
Sammed Badde <sammedbadde10@gmail.com>
2026-03-27 10:07:14 +0530

    Update declaration_in_C.c

---
+++ C Practice/Basics of C (Practice)/declaration_in_C.c
@@ -0,0 +1,21 @@
+//in this code we are going  to learn how to store and declaration in C
+#include <stdio.h>
+int main(){
+//variables and datatypes example
+int age=20;
+float height= 5.9;
+char initial ='S';
+
+printf("Mera naam %c se start hota hai.\n",initial);
+printf("Meri Age  %d hai.\n",age);
+printf("Meri height %.1f feet hai.\n",height);
+
+return 0;
+    
+}
+
+//Expected output
+// Mera naam S se start hota hai.
+// Meri Age  20 hai.
+// Meri height 5.9 feet hai.
+
